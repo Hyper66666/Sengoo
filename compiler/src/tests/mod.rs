550ece937156da0e1e565d8980dfa3d6f2d27285
Hyper66666 <2247081184@qq.com>
2026-03-23 15:49:52 +0800

    refactor: extract async frame layout helpers

--- compiler/src/tests/mod.rs
+++ compiler/src/tests/mod.rs
@@ -3,6 +3,7 @@
 pub mod array_assign_tests;
 pub mod async_tests;
 pub mod async_cfg_helpers_tests;
+pub mod async_frame_helpers_tests;
 pub mod async_origin_helpers_tests;
 pub mod cast_semantics_tests;
 pub mod class_inheritance_tests;
