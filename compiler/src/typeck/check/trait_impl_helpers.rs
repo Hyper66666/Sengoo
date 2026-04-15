86c14abb25ca15bf24e6f287c84f522813101ef7
Hyper66666 <2247081184@qq.com>
2026-04-15 15:07:02 +0800

    refactor: extract typeck trait impl helpers

---
+++ compiler/src/typeck/check/trait_impl_helpers.rs
@@ -0,0 +1,166 @@
+use super::*;
+use crate::typeck::r#trait::{type_key, FunctionTy, ImplInfo, MethodSig, TraitInfo};
+
+impl TypeChecker {
+    pub(super) fn check_trait_decl(&mut self, trait_decl: &Trait) -> Result<()> {
+        self.env.push_scope();
+        self.bind_type_params_with_meta(&trait_decl.type_params)?;
+
+        let mut trait_info = TraitInfo::new(
+            trait_decl.name.name.clone(),
+            trait_decl
+                .type_params
+                .iter()
+                .map(|tp| tp.name.name.clone())
+                .collect(),
+            matches!(trait_decl.vis, Visibility::Public),
+        );
+
+        for item in &trait_decl.items {
+            match item {
+                TraitItem::Function(method) => {
+                    self.env.push_scope();
+                    let method_generic_meta = self.bind_type_params_with_meta(&method.type_params)?;
+                    let mut param_types = Vec::new();
+                    let mut has_self = false;
+
+                    for param in &method.params {
+                        if param.name.name == "self" {
+                            has_self = true;
+                        } else {
+                            let ty = self.check_type(&param.ty)?;
+                            param_types.push(ty);
+                        }
+                    }
+
+                    let ret_ty = if let Some(ret) = &method.return_type {
+                        self.check_type(ret)?
+                    } else {
+                        self.env.unit_ty()
+                    };
+
+                    let has_default = !method.body.stmts.is_empty();
+                    let sig = if has_default {
+                        MethodSig::with_default(
+                            has_self,
+                            param_types,
+                            ret_ty,
+                            method_generic_meta.iter().map(|meta| meta.var_id).collect(),
+                        )
+                    } else {
+                        MethodSig::new(
+                            has_self,
+                            param_types,
+                            ret_ty,
+                            method_generic_meta.iter().map(|meta| meta.var_id).collect(),
+                        )
+                    };
+                    trait_info.add_method(method.name.name.clone(), sig);
+                    self.env.pop_scope();
+                }
+                TraitItem::Const(const_decl) => {
+                    let ty = self.check_type(&const_decl.ty)?;
+                    trait_info.add_const(const_decl.name.name.clone(), ty);
+                }
+                TraitItem::Type(type_alias) => {
+                    trait_info.add_assoc_type(type_alias.name.name.clone());
+                }
+            }
+        }
+
+        self.trait_registry.register(trait_info);
+
+        self.env.pop_scope();
+        Ok(())
+    }
+
+    pub(super) fn check_impl_decl(&mut self, impl_decl: &Impl) -> Result<()> {
+        self.env.push_scope();
+        self.bind_type_params_with_meta(&impl_decl.type_params)?;
+
+        let target_ty = self.check_type(&impl_decl.target_type)?;
+        let target_key = type_key(&target_ty);
+
+        let trait_name = impl_decl
+            .trait_path
+            .as_ref()
+            .and_then(|p| p.as_simple())
+            .map(|s| s.name.clone());
+
+        let mut impl_info = ImplInfo::new(target_ty.clone(), trait_name);
+
+        for item in &impl_decl.items {
+            self.env.push_scope();
+            let method_generic_meta = self.bind_type_params_with_meta(&item.type_params)?;
+            let mut param_types = Vec::new();
+            let mut has_self = false;
+            for param in &item.params {
+                if param.name.name == "self" {
+                    has_self = true;
+                } else {
+                    let ty = self.check_type(&param.ty)?;
+                    param_types.push(ty);
+                }
+            }
+            let ret_ty = if let Some(ret) = &item.return_type {
+                self.check_type(ret)?
+            } else {
+                self.env.unit_ty()
+            };
+            impl_info.add_method(
+                item.name.name.clone(),
+                FunctionTy::with_generic_params(
+                    has_self,
+                    param_types,
+                    ret_ty,
+                    method_generic_meta.iter().map(|meta| meta.var_id).collect(),
+                ),
+            );
+            self.env.pop_scope();
+        }
+
+        if let Some(trait_name) = impl_info.trait_name.clone() {
+            if let Some(trait_info) = self.trait_registry.get(&trait_name) {
+                let mut missing_methods = Vec::new();
+
+                for (method_name, method_sig) in &trait_info.methods {
+                    if !impl_info.has_method(method_name) {
+                        if method_sig.has_default {
+                            impl_info.add_method(
+                                method_name.clone(),
+                                FunctionTy::with_generic_params(
+                                    method_sig.has_self,
+                                    method_sig.param_types.clone(),
+                                    method_sig.return_type.clone(),
+                                    method_sig.generic_params.clone(),
+                                ),
+                            );
+                        } else {
+                            missing_methods.push(method_name.clone());
+                        }
+                    }
+                }
+
+                if !missing_methods.is_empty() {
+                    missing_methods.sort();
+                    self.env.pop_scope();
+                    let err = TypeckError::Other(format!(
+                        "impl {} for {} is missing required trait methods: {}",
+                        trait_name,
+                        target_key,
+                        missing_methods.join(", ")
+                    ));
+                    return Err(CompileError::TypeckError(err));
+                }
+            }
+
+            self.impl_registry
+                .register_trait_impl(trait_name, target_key, impl_info);
+        } else {
+            self.impl_registry.register_inherent(target_key, impl_info);
+        }
+
+        self.env.pop_scope();
+        Ok(())
+    }
+}
