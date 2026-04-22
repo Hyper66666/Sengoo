6b66f9a9051a9e70242df0234bdc4db3ddb38e21
Hyper66666 <2247081184@qq.com>
2026-04-22 16:04:00 +0800

    refactor: extract codegen terminator helpers

---
+++ compiler/src/codegen/terminator_helpers.rs
@@ -0,0 +1,226 @@
+use super::*;
+
+impl Codegen {
+    pub(super) fn codegen_terminator(
+        &mut self,
+
+        terminator: &mir::Terminator,
+
+        mir_fn: &MirFunction,
+    ) -> Result<(), String> {
+        match terminator {
+            mir::Terminator::Return(value) => {
+                if let Some(v) = value {
+                    let ty = self.get_local_type(mir_fn, *v);
+
+                    // `main` returning unit still lowers to exit code 0.
+                    let is_main_returning_unit =
+                        mir_fn.name == "main" && matches!(ty, MIRType::Unit);
+
+                    if is_main_returning_unit {
+                        self.emit_indent();
+
+                        self.ir.push_str("ret i64 0\n");
+                    } else {
+                        // 闁哄鏅滈弻銊ッ洪弽顓燁棃?unit 闂佺锕ら崥瀣渻閸岀偞鏅€光偓閸曘劌浜炬慨妯虹－缁?operand_value 闂佸湱顭堥ˇ闈涖€掗崼鏇炵闁绘鍎ょ粊浼村级閳哄倹鐓ユ繛鍙夌墱閳ь剟娼х€氼剟鎮洪妸鈺侀棷闁靛绲洪崑?
+                        let reg = self.operand_value(*v, mir_fn);
+
+                        // unit 闂?LLVM 婵炴垶鎼╅崢楣冨极?i8 0 闁荤偞绋忛崝搴ㄥΦ濮樿埖鏅€光偓閸愶絽浜鹃悘鐐跺亹閻熸繈鏌?void闂?
+                        let llvm_ty = if matches!(ty, MIRType::Unit) {
+                            "i8".to_string()
+                        } else {
+                            self.mir_type_to_llvm_cached(ty)
+                        };
+
+                        self.emit_indent();
+
+                        self.ir.push_str(&format!("ret {} {}\n", llvm_ty, reg));
+                    }
+                } else {
+                    // 闂佸搫鍟版慨闈浳熸径濠庡殨闊洦姘ㄧ粻鏌ユ煕閵壯冧粶闁逞屽墮閸氬顪冮崒鐐存櫖閻庣懓瓒卆in` 婵帗绋掗…鍫ヮ敇缂佹ɑ浜ら柡鍌涘缁€鈧?0闂?
+                    if mir_fn.name == "main" {
+                        self.emit_indent();
+
+                        self.ir.push_str("ret i64 0\n");
+                    } else {
+                        self.emit_indent();
+
+                        self.ir.push_str("ret void\n");
+                    }
+                }
+            }
+
+            mir::Terminator::Goto(target) => {
+                self.emit_indent();
+
+                self.ir.push_str(&format!("br label %bb_{}\n", target));
+            }
+
+            mir::Terminator::If {
+                cond: condition,
+
+                then_block,
+
+                else_block,
+            } => {
+                self.emit_indent();
+
+                let cond_reg = self.operand_value(*condition, mir_fn);
+
+                self.ir.push_str(&format!(
+                    "br i1 {}, label %bb_{}, label %bb_{}\n",
+                    cond_reg, then_block, else_block
+                ));
+            }
+
+            mir::Terminator::Switch {
+                discr,
+
+                targets,
+
+                otherwise,
+            } => {
+                self.emit_indent();
+
+                let discr_reg = self.operand_value(*discr, mir_fn);
+
+                // 闂佹眹鍨婚崰鎰板垂?switch 闂佸湱顭堝ú锝夊箮?
+
+                self.ir.push_str(&format!(
+                    "switch i64 {}, label %bb_{} [",
+                    discr_reg, otherwise
+                ));
+
+                // 濠电儑缍€椤曆勬叏閻愭畫鎺曠疀鎼淬劌娈?case
+
+                for (value, target) in targets {
+                    self.ir.push('\n');
+
+                    self.emit_indent();
+
+                    self.ir
+                        .push_str(&format!("  i64 {}, label %bb_{}", value, target));
+                }
+
+                self.ir.push('\n');
+
+                self.emit_indent();
+
+                self.ir.push_str("]\n");
+            }
+
+            mir::Terminator::Break { target } => {
+                self.emit_indent();
+
+                self.ir.push_str(&format!("br label %bb_{}\n", target));
+            }
+
+            mir::Terminator::Continue { target } => {
+                self.emit_indent();
+
+                self.ir.push_str(&format!("br label %bb_{}\n", target));
+            }
+
+            mir::Terminator::Unreachable => {
+                self.emit_indent();
+
+                self.ir.push_str("unreachable\n");
+            }
+
+            mir::Terminator::Call {
+                func,
+
+                args,
+
+                destination,
+
+                target,
+            } => {
+                let dest = self.local_name(*destination);
+
+                let dest_ty = self.get_local_type(mir_fn, *destination);
+
+                let ret_ty = self.mir_type_to_llvm_cached(dest_ty);
+
+                let arg_strs: Vec<String> = args
+                    .iter()
+                    .map(|arg| match arg {
+                        mir::CallArg::Local(local) => {
+                            let arg_ty = self.get_local_type(mir_fn, *local);
+
+                            let llvm_arg_ty = self.mir_type_to_llvm_cached(arg_ty);
+
+                            let val = self.operand_value(*local, mir_fn);
+
+                            format!("{} {}", llvm_arg_ty, val)
+                        }
+
+                        mir::CallArg::Constant(constant) => match constant {
+                            MirConstant::Int(n) => format!("i64 {}", n),
+
+                            MirConstant::Bool(b) => format!("i1 {}", if *b { 1 } else { 0 }),
+
+                            MirConstant::Float(f) => format!("double {}", f),
+
+                            MirConstant::String(s) => format!(
+                                "i8* @.str.{}",
+                                self.strings.iter().position(|x| x == s).unwrap_or(0)
+                            ),
+
+                            _ => "i64 0".to_string(),
+                        },
+                    })
+                    .collect();
+
+                self.emit_indent();
+
+                if ret_ty == "void" {
+                    self.ir
+                        .push_str(&format!("call void @{}({})\n", func, arg_strs.join(", ")));
+                } else {
+                    self.ir.push_str(&format!(
+                        "{} = call {} @{}({})\n",
+                        dest,
+                        ret_ty,
+                        func,
+                        arg_strs.join(", ")
+                    ));
+                }
+
+                self.emit_indent();
+
+                self.ir.push_str(&format!("br label %bb_{}\n", target));
+            }
+
+            mir::Terminator::Suspend {
+                poll_func,
+                future_handle,
+                destination,
+                ready_block,
+                pending_block,
+            } => {
+                let handle_val = self.operand_value(*future_handle, mir_fn);
+                let dest = self.local_name(*destination);
+
+                self.emit_indent();
+                self.ir.push_str(&format!(
+                    "{} = call i64 @{}(i64 {})\n",
+                    dest, poll_func, handle_val
+                ));
+
+                let cmp = format!("{}_cmp", dest);
+                self.emit_indent();
+                self.ir
+                    .push_str(&format!("{} = icmp eq i64 {}, 1\n", cmp, dest));
+
+                self.emit_indent();
+                self.ir.push_str(&format!(
+                    "br i1 {}, label %bb_{}, label %bb_{}\n",
+                    cmp, ready_block, pending_block
+                ));
+            }
+        }
+
+        Ok(())
+    }
+}
